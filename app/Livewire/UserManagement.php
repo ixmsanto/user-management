86939b8fb115ee2b0cba6a1441227d6f901df4c7
ixmsanto <santhoshjeje@gmail.com>
2025-05-01 16:54:05 +0530

    feat: livewire usermanagement created

---
+++ app/Livewire/UserManagement.php
@@ -0,0 +1,90 @@
+<?php
+namespace App\Livewire;
+
+use Livewire\Component;
+use App\Models\User;
+
+class UserManagement extends Component
+{
+    public $users;
+    public $name, $email, $password, $role;
+    public $editing = false;
+    public $editUserId;
+
+    protected $rules = [
+        'name' => 'required|string|max:255',
+        'email' => 'required|string|email|max:255|unique:users,email',
+        'password' => 'required|string|min:8',
+        'role' => 'required|in:admin,user',
+    ];
+
+    public function mount()
+    {
+        $this->users = User::with('roles')->get();
+    }
+
+    public function render()
+    {
+        return view('livewire.user-management');
+    }
+
+    public function create()
+    {
+        $this->validate();
+
+        $user = User::create([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => bcrypt($this->password),
+        ]);
+
+        $user->assignRole($this->role);
+        $this->resetForm();
+        $this->users = User::with('roles')->get();
+    }
+
+    public function edit($id)
+    {
+        $user = User::findOrFail($id);
+        $this->editUserId = $id;
+        $this->name = $user->name;
+        $this->email = $user->email;
+        $this->role = $user->roles->first()->name;
+        $this->editing = true;
+
+        $this->rules['email'] = 'required|string|email|max:255|unique:users,email,' . $id;
+        $this->rules['password'] = 'nullable|string|min:8';
+    }
+
+    public function update()
+    {
+        $this->validate();
+
+        $user = User::findOrFail($this->editUserId);
+        $user->update([
+            'name' => $this->name,
+            'email' => $this->email,
+            'password' => $this->password ? bcrypt($this->password) : $user->password,
+        ]);
+
+        $user->syncRoles($this->role);
+        $this->resetForm();
+        $this->users = User::with('roles')->get();
+    }
+
+    public function delete($id)
+    {
+        User::findOrFail($id)->delete();
+        $this->users = User::with('roles')->get();
+    }
+
+    public function resetForm()
+    {
+        $this->name = '';
+        $this->email = '';
+        $this->password = '';
+        $this->role = '';
+        $this->editing = false;
+        $this->editUserId = null;
+    }
+}
