697fbbb2e91a96804e12f2b7d35a1ca442033e2f
ixmsanto <santhoshjeje@gmail.com>
2025-05-01 17:00:09 +0530

    admin.users view blade created

---
+++ resources/views/admin/users.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Admin Dashboard - User Management</h1>
+            @livewire('user-management')
+        </div>
+    </div>
+@endsection
