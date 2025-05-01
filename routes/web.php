2a04b5c7ca80616bc8dbf0461b4c1227008ebf43
ixmsanto <santhoshjeje@gmail.com>
2025-05-01 17:49:40 +0530

    feat: middleware admin route created

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,17 @@
 <?php
-
 use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Auth;
+use App\Http\Controllers\UserController;
 
-Route::get('/', function () {
-    return view('welcome');
+Auth::routes();
+
+Route::get('/', fn() => view('welcome'))->name('home');
+
+Route::middleware(['auth', 'role:admin'])->group(function () {
+    Route::get('/admin/users', [UserController::class, 'index'])->name('admin.users');
+    Route::post('/admin/users', [UserController::class, 'store'])->name('admin.users.store');
+    Route::put('/admin/users/{user}', [UserController::class, 'update'])->name('admin.users.update');
+    Route::delete('/admin/users/{user}', [UserController::class, 'destroy'])->name('admin.users.destroy');
 });
 
 Auth::routes();
